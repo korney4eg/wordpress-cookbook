965464c6d82773d7823c4cd4640b9df2faf341be
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-02-23 15:58:32 +0300

    wp ip address

--- recipes/default.rb
+++ recipes/default.rb
@@ -125,7 +125,7 @@ wp_cli_command 'core install' do
   args(
      'path' => '/var/www/wordpress',
      'allow-root' => '',
-     'url' => node['ipaddress'],
+     'url' => node['wordpress']['ip'],
      'title' => 'Test-Site',
      'admin_name' => 'admin',
      'admin_email' => 'email@mail.mail',
