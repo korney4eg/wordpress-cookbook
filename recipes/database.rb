9cc79664ef20bcc299142a2678f408a5c31d57fc
Szymon Janczy <Szymon_Janczy@epam.com>
2016-07-21 15:20:21 +0200

    Make this version of cookbook working with specified 	dependecies (mostly having optimistic version constraint). 	Tested with Ubuntu 14.04.
    	modified:   Berksfile
    			- force using "mysql" cookbook in major version 7,
    			  as 8 is still having unsolved issue (432) with
    			  unspported action for mysql_client resource:
    			  "mysql_client does not have action :create"
    	modified:   attributes/default.rb
    			- use Loopback`s ip address "127.0.0.1" rather
    			  than 'localhost' as a value of db/host
    			  atribute to satisfy the specifc usage of mysql
    			  client by "mysql" cookbook (it requires -h or -S
    			  switches). See more at cookbook`s FAQ
    	modified:   libraries/helpers.rb
    			- introduce 'host' parameter to the 'make_db_query' method,
    			  to assure proper handling of mysql client
    			  (binding over network to mysqld service)
    	modified:   recipes/database.rb
    			- adapt usage of 'mysql' cookbook as since version
    			  6.0.0 it provides LWRP instead of recipes
    	modified:   metadata.rb
    			- add specific version constraint, to avoid
    			  ambiguity for Berkshelf when solving cookbook`s
    			  dependencies

--- recipes/database.rb
+++ recipes/database.rb
@@ -19,18 +19,28 @@
 # limitations under the License.
 #
 
-include_recipe "mysql::client" unless platform_family?('windows') # No MySQL client on Windows
+mysql_client 'default' do
+  action :create
+  not_if { node['platform_family'] == 'windows' }
+end
+
+mysql_service 'wordpress' do
+  port '3306'
+  version '5.5'
+  initial_root_password node['mysql']['server_root_password']
+  action [:create, :start]
+end
 
 ::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
-::Chef::Recipe.send(:include, Wordpress::Helpers)
 
 node.set_unless['wordpress']['db']['pass'] = secure_password
 node.save unless Chef::Config[:solo]
 
+::Chef::Recipe.send(:include, Wordpress::Helpers)
+
 db = node['wordpress']['db']
 
 if is_local_host? db['host']
-  include_recipe "mysql::server"
 
   mysql_bin = (platform_family? 'windows') ? 'mysql.exe' : 'mysql'
   user = "'#{db['user']}'@'#{db['host']}'"
@@ -44,25 +54,25 @@ if is_local_host? db['host']
 
   execute "Create WordPress MySQL User" do
     action :run
-    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], create_user)}"
-    only_if { `#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], user_exists)}`.empty? }
+    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db['host'], create_user)}"
+    only_if { `#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db['host'], user_exists)}`.empty? }
   end
 
   execute "Grant WordPress MySQL Privileges" do
     action :run
-    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], grant_privileges)}"
-    only_if { `#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], privileges_exist)}`.empty? }
+    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db['host'], grant_privileges)}"
+    only_if { `#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db['host'], privileges_exist)}`.empty? }
     notifies :run, "execute[Flush MySQL Privileges]"
   end
 
   execute "Flush MySQL Privileges" do
     action :nothing
-    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], flush_privileges)}"
+    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db['host'], flush_privileges)}"
   end
 
   execute "Create WordPress Database" do
     action :run
-    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], create_db)}"
-    only_if { `#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db_exists)}`.empty? }
+    command "#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db['host'], create_db)}"
+    only_if { `#{mysql_bin} #{::Wordpress::Helpers.make_db_query("root", node['mysql']['server_root_password'], db['host'], db_exists)}`.empty? }
   end
 end
